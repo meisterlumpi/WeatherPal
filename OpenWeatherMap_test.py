f713c42b255486f1328d8c980d17ff21b452c3be
meisterlumpi <meisterlumpi@gmail.com>
2016-05-14 14:44:38 +0200

    update

--- OpenWeatherMap_test.py
+++ OpenWeatherMap_test.py
@@ -1,4 +1,5 @@
 from pyowm import OWM
+import unicodedata
 
 API_key = '<enter api key here>'
 
@@ -13,45 +14,54 @@ if owm.is_API_online():
     obs = owm.weather_at_place(location_str)
     w = obs.get_weather()
 
-    print(w.get_reference_time())  # get time of observation in GMT UNIXtime
-    print(w.get_reference_time(timeformat='iso'))             # ...or in ISO8601
-    print(w.get_clouds())                                     # Get cloud coverage
-    print(w.get_rain())                                       # Get rain volume
-    print(w.get_snow())                                       # Get snow volume
-    print(w.get_wind())                                       # Get wind degree and speed
-    print(w.get_humidity())                                   # Get humidity percentage
-    print(w.get_pressure())                                   # Get atmospheric pressure
-    print(w.get_temperature())                                # Get temperature in Kelvin
-    print(w.get_temperature(unit='celsius'))                  # ... or in Celsius degs
-    print(w.get_temperature('fahrenheit'))                    # ... or in Fahrenheit degs
-    print(w.get_status())                                     # Get weather short status
-    print(w.get_detailed_status())                           # Get detailed weather status
-    print(w.get_weather_code())                               # Get OWM weather condition code
-    print(w.get_weather_icon_name())                          # Get weather-related icon name
-    print(w.get_sunrise_time())                               # Sunrise time (GMT UNIXtime or ISO 8601)
-    print(w.get_sunset_time('iso'))                           # Sunset time (GMT UNIXtime or ISO 8601)
+    print('reference_time())                 ', w.get_reference_time())                             # get time of observation in GMT UNIXtime
+    print('reference_time(timeformat="iso")) ', w.get_reference_time(timeformat="iso"))             # ...or in ISO8601
+    print('clouds())                         ', w.get_clouds())                                     # Get cloud coverage
+    print('rain())                           ', w.get_rain())                                       # Get rain volume
+    print('snow())                           ', w.get_snow())                                       # Get snow volume
+    print('wind())                           ', w.get_wind())                                       # Get wind degree and speed
+    print('humidity())                       ', w.get_humidity())                                   # Get humidity percentage
+    print('pressure())                       ', w.get_pressure())                                   # Get atmospheric pressure
+    print('temperature())                    ', w.get_temperature())                                # Get temperature in Kelvin
+    print('temperature(unit="celsius"))      ', w.get_temperature(unit="celsius"))                  # ... or in Celsius degs
+    print('temperature("fahrenheit"))        ', w.get_temperature("fahrenheit"))                    # ... or in Fahrenheit degs
+    print('status())                         ', w.get_status())                                     # Get weather short status
+    print('detailed_status())                ', w.get_detailed_status())                            # Get detailed weather status
+    print('weather_code())                   ', w.get_weather_code())                               # Get OWM weather condition code
+    print('weather_icon_name())              ', w.get_weather_icon_name())                          # Get weather-related icon name
+
+
+    base_url = 'http://openweathermap.org/img/w/'  # 10d.png
+    img_file_name = unicodedata.normalize('NFKD', w.get_weather_icon_name()).encode('ascii', 'ignore')
+    img_file_url = base_url + img_file_name + '.png'
+
+    print('weather icon url: ', img_file_url)
+
+    print('sunrise_time())                   ', w.get_sunrise_time())                               # Sunrise time (GMT UNIXtime or ISO 8601)
+    print('sunset_time("iso"))               ', w.get_sunset_time("iso"))                           # Sunset time (GMT UNIXtime or ISO 8601)
 
     print('\n')
 
     fc = owm.three_hours_forecast(location_str)
     f = fc.get_forecast()
-    print(f.get_reception_time())                           # UNIX GMT time
-    print(f.get_reception_time('iso'))                      # ISO8601
-    print(f.get_interval())
+    print('forecast:')
+    print('reception_time())      ', f.get_reception_time())                           # UNIX GMT time
+    print('reception_time("iso")) ', f.get_reception_time("iso"))                      # ISO8601
+    print('interval())            ', f.get_interval())
     print(len(f))
-    print(f.get_location())
+    print('location()             ', f.get_location())
 
     lst = f.get_weathers()
     for weather in f:
-        print (weather.get_reference_time('iso'),weather.get_status())
+        print (weather.get_reference_time("iso"), weather.get_status())
 
     print('\n')
 
-    print(fc.will_have_rain())
-    print(fc.will_have_sun())
-    print(fc.will_have_fog())
-    print(fc.will_have_clouds())
-    print(fc.will_have_snow())
+    print('will_have_rain()   ', fc.will_have_rain())
+    print('will_have_sun()    ', fc.will_have_sun())
+    print('will_have_fog()    ', fc.will_have_fog())
+    print('will_have_clouds() ', fc.will_have_clouds())
+    print('will_have_snow()   ', fc.will_have_snow())
 
 else:
     print('Service is offline')
